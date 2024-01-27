46b36c75195ca86c51163bd17127de1807d50708
Matheus <matheusverbanek@gmail.com>
2024-01-27 02:53:58 -0300

    dependecy injection

---
+++ src/main/java/com/verbanek/course/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.verbanek.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.verbanek.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	
+	
+}
